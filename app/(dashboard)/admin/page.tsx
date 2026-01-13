490d6f4fb4f2677246a470035e1b6ec753693617
Martin Elmars <121293732+elmars-hub@users.noreply.github.com>
2026-01-13 14:03:07 +0100

    feat: add AttendanceChart component and integrate into AdminDashboard

--- app/(dashboard)/admin/page.tsx
+++ app/(dashboard)/admin/page.tsx
@@ -1,10 +1,12 @@
+import AttendanceChart from "@/components/charts/AttendanceChart";
+import CountChart from "@/components/charts/CountChart";
 import UserCard from "@/components/UserCard";
 
 export default function AdminDashboard() {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
       {/* Left */}
-      <div className="w-full lg:w-2/3">
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
         {/* USER CARD */}
 
         <div className="flex gap-4 justify-between flex-wrap">
@@ -13,10 +15,27 @@ export default function AdminDashboard() {
           <UserCard type="student" />
           <UserCard type="staff" />
         </div>
+
+        {/* MIDDLE CHART */}
+
+        <div className="flex gap-4 flex-col lg:flex-row">
+          {/* count chart */}
+          <div className="w-full lg:w-1/3 h-112.5">
+            <CountChart />
+          </div>
+
+          {/* attendance chart */}
+          <div className="w-full lg:w-2/3 h-122.5">
+            <AttendanceChart />
+          </div>
+
+          {/* finance chart */}
+          <div className="w-full"></div>
+        </div>
       </div>
 
       {/* Right */}
-      <div className=""></div>
+      <div className="w-full lg:w-1/3"></div>
     </div>
   );
 }
