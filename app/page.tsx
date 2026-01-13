46025e689b092ae2aae9d07341682530b3181982
Martin Elmars <121293732+elmars-hub@users.noreply.github.com>
2026-01-13 11:51:33 +0100

    refactor: remove unused imports and simplify Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,3 @@
 export default function Home() {
   return <div className="f"></div>;
 }
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Lama Dev School Management Dashboard",
-  description: "Next.js School Management System",
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
-}
