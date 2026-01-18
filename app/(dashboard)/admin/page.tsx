751209d9936b98bad685e364accd3eb92adad4df
Martin Elmars <121293732+elmars-hub@users.noreply.github.com>
2026-01-18 15:32:50 +0100

    feat: update

--- app/(dashboard)/admin/page.tsx
+++ app/(dashboard)/admin/page.tsx
@@ -8,39 +8,33 @@ import UserCard from "@/components/UserCard";
 export default function AdminDashboard() {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
-      {/* Left */}
+      {/* LEFT */}
       <div className="w-full lg:w-2/3 flex flex-col gap-8">
-        {/* USER CARD */}
-
+        {/* USER CARDS */}
         <div className="flex gap-4 justify-between flex-wrap">
-          <UserCard type="admin" />
-          <UserCard type="teacher" />
           <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
           <UserCard type="staff" />
         </div>
-
-        {/* MIDDLE CHART */}
-
+        {/* MIDDLE CHARTS */}
         <div className="flex gap-4 flex-col lg:flex-row">
-          {/* count chart */}
+          {/* COUNT CHART */}
           <div className="w-full lg:w-1/3 h-112.5">
             <CountChart />
           </div>
-
-          {/* attendance chart */}
-          <div className="w-full lg:w-2/3 h-122.5">
+          {/* ATTENDANCE CHART */}
+          <div className="w-full lg:w-2/3 h-112.5">
             <AttendanceChart />
           </div>
-
-          {/* finance chart */}
-          <div className="w-full h-125">
-            <FinanceChart />
-          </div>
+        </div>
+        {/* BOTTOM CHART */}
+        <div className="w-full h-125">
+          <FinanceChart />
         </div>
       </div>
-
-      {/* Right */}
-      <div className="w-full lg:w-1/3">
+      {/* RIGHT */}
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
         <EventCalendar />
         <Announcements />
       </div>
